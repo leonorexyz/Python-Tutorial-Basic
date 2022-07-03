1d359e669013d02d1b60dcd1f2af1ab139731898
Hengki Pranoto <lunatic.icee@gmail.com>
2022-07-03 23:34:29 +0700

    new-update

--- 5 - Branching/Main.py
+++ 5 - Branching/Main.py
@@ -1,3 +1,4 @@
+'''
 # Branching - A way to decide an argument / decision
 number = int(input("Masukkan number : "))
 if(number % 2 == 0):
@@ -16,3 +17,19 @@ elif(marks >= 40):
     print("D")
 else:
     print("E")
+'''
+
+#  Exercise - Simple Calculator
+number1 = int(input("Insert number 1 : ")) #operand
+operator = input("Insert operator : ")
+number2 = int(input("Insert number 2 : "))
+
+if(operator == "+"):
+    print(f"Result : {number1+number2}")
+if(operator == "-"):
+    print(f"Result : {number1-number2}")
+if(operator == "*"):
+    print(f"Result : {number1*number2}")
+if(operator == "/"):
+    print(f"Result : {number1/number2}")
+
--- 6 - Looping/Main.py
+++ 6 - Looping/Main.py
@@ -1,16 +1,17 @@
 # Looping - A way to iterate something
-# for loop, for i in range(n), let's say n is 11 so it will iterate from 0-10, for loop is used when we know the range
+# for loop, for i in range(n), let's say n is 11 so it will iterate from 0-10, for loop is used when we know the range, you can also define the start step, end step (remember it's always n+1), and the step
 # while loop, while loop is used when we don't know how much we want to iterate, it'll always loop until the condition break
 
+'''
 n = 10
 result = 1
-for i in range(11):
+for i in range(0,n+1):
     if(i!=0):
         result *= i
 print(result)
 
-n = 6
-for i in range(n):
+n = 10
+for i in range(1,n+1):
     for j in range(i):
         print("*", end=" ")
     print()
@@ -21,3 +22,39 @@ while n: #if n is <1 the condition will break, remember in programming 1 = True,
     result *= n
     n -= 1
 print(result)
+
+fact = int(input("Insert number : "))
+result = 1
+for i in range(fact,0,-1):
+    result *= i
+    if(i == 1):
+        print(str(i)+ " = ",end="")
+    else:
+        print(str(i)+ " * ",end="")
+print(result)
+'''
+
+# fast way to implement triangle star - exercise
+print("Choose Shape".center(20,"-"))
+print("1. Square")
+print("2. Triangle")
+print("3. Reverse Triangle")
+print("4. Diamond")
+shape = int(input("Choose shape : "))
+size = int(input("Choose size : "))
+if(shape == 1):
+    for i in range(1, size+1):
+        print("*" * size)
+if(shape == 2):
+    for i in range(1, size+1):
+        print("*" * i)
+if(shape == 3):
+    for i in range(size, 0, -1):
+        print("*" * i)
+if(shape == 4):
+    for i in range(1, size*2):
+        print(" "*(size-(int(size)-abs(i-size))),end="")
+        print("*"*(int(size)-abs(i-size)))
+        print(" "*(size-(int(size)-abs(i-size))),end="")
+        # print((int(size)-abs(i-size)))
+        # print(size-(int(size)-abs(i-size)))
